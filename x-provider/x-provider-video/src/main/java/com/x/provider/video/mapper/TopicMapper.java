a561e78bee69b38bb088f5d03edb67cd328a2778
yjjhkyq <liushenyi163@163.com>
2021-09-05 22:47:43 +0800

    话题管理

---
+++ x-provider/x-provider-video/src/main/java/com/x/provider/video/mapper/TopicMapper.java
@@ -0,0 +1,9 @@
+package com.x.provider.video.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.x.provider.video.model.domain.Topic;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicMapper extends BaseMapper<Topic> {
+}
